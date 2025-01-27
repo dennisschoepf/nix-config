26746d649253036f56da78e5b018dc4aa78bf864
Dennis <me@dnsc.io>
2025-01-27 23:17:23 +0100

    Removes echo on envsource

--- home/modules/fish/default.nix
+++ home/modules/fish/default.nix
@@ -18,7 +18,6 @@
         for line in (cat $argv | grep -v '^#')
             set item (string split -m 1 '=' $line)
             set -gx $item[1] $item[2]
-            echo "Exported key $item[1]"
         end
       '';
       fish_greeting = "fortune -a";
