21596087a50a385c9b041942d6b299b7a1393b02
dnscio <dnscio@noreply.codeberg.org>
2025-02-04 07:14:59 +0000

    Update home/modules/nvim/config/lua/plugins/lsp.lua

--- home/modules/nvim/config/lua/plugins/lsp.lua
+++ home/modules/nvim/config/lua/plugins/lsp.lua
@@ -161,7 +161,6 @@ return {
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
         vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<space>f", function()
           vim.lsp.buf.format({ async = true })
