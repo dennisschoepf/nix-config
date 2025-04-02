a0598223697125853129655a0c8672baf9d53074
Dennis <me@dnsc.io>
2025-04-02 19:47:58 +0200

    Removes key overwrites

--- home/modules/nvim/config/lua/plugins/vimwiki.lua
+++ home/modules/nvim/config/lua/plugins/vimwiki.lua
@@ -4,7 +4,4 @@ return {
     vim.g.vimwiki_list = { { path = "~/notes", syntax = "markdown", ext = "md" } }
     vim.g.vimwiki_map_prefix = "<Leader>o"
   end,
-  keys = {
-    { "<leader>d", "<cmd>90vsplit | VimwikiMakeDiaryNote<cr>", desc = "Open today's diary in split", mode = "n" },
-  },
 }
