0b367e16560f3daa97cf66dac0ddc5a2559279dc
Dennis <me@dnsc.io>
2025-04-09 18:06:43 +0200

    Moves to blink for completions

---
+++ home/modules/nvim/config/lua/plugins/blink.lua
@@ -0,0 +1,28 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  version = "1.*",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = "super-tab" }, -- 'default' | 'super-tab' | 'enter'
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    -- (Default) Only show the documentation popup when manually triggered
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    completion = { documentation = { auto_show = false } },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      per_filetype = { sql = { "dadbod" } },
+      providers = {
+        dadbod = { module = "vim_dadbod_completion.blink" },
+      },
+    },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    cmdline = {
+      completion = { menu = { auto_show = true } },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
