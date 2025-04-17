bba0603fae519dc82968a7d1dc0c5113ffafc433
dnscio <dnscio@noreply.codeberg.org>
2025-04-17 15:03:47 +0000

    Update home/modules/nvim/config/lsp/ts_ls.lua

--- home/modules/nvim/config/lsp/ts_ls.lua
+++ home/modules/nvim/config/lsp/ts_ls.lua
@@ -1,7 +1,4 @@
-local dnscUtils = require("dnsc.utils")
-
 return {
-  init_options = { hostInfo = "neovim" },
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = {
     "javascript",
@@ -13,20 +10,11 @@ return {
   },
   root_markers = { "tsconfig.json", "package.json", ".git" },
   init_options = {
+    hostInfo = "neovim",
     preferences = {
       importModuleSpecifierPreference = "relative",
       importModuleSpecifierEnding = "minimal",
     },
-  },
-  handlers = {
-    ["textDocument/definition"] = function(err, result, method, ...)
-      if vim.tbl_islist(result) and #result > 1 then
-        local filtered_result = dnscUtils.filter(result, dnscUtils.filterReactDTS)
-        return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-      end
-
-      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-    end,
   },
   single_file_support = true,
 }
