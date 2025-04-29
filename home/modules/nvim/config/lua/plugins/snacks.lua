55a83977232f79f2ae9213b82c3f673ac98eef4b
dnscio <dnscio@noreply.codeberg.org>
2025-04-29 10:27:56 +0000

    Update home/modules/nvim/config/lua/plugins/snacks.lua
    Fixes `react.d.ts`

--- home/modules/nvim/config/lua/plugins/snacks.lua
+++ home/modules/nvim/config/lua/plugins/snacks.lua
@@ -1,4 +1,13 @@
 ---@module 'snacks'
+
+local filter_lsp_definitions = function(item)
+  if item.file:match("/react/ts5.0/") then
+    return false
+  end
+
+  return true
+end
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
@@ -8,7 +17,7 @@ return {
     dashboard = {
       enabled = false,
       sections = {
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "keys",   gap = 1, padding = 1 },
         { section = "startup" },
         {
           section = "terminal",
@@ -39,30 +48,30 @@ return {
   -- stylua: ignore
   keys = {
     -- Git
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Show git blame for current line" },
-    { "<leader>gl", function() Snacks.gitbrowse() end, desc = "Open link to line at git remote" },
+    { "<leader>gg",       function() Snacks.lazygit() end,                                                                desc = "Lazygit" },
+    { "<leader>gb",       function() Snacks.git.blame_line() end,                                                         desc = "Show git blame for current line" },
+    { "<leader>gl",       function() Snacks.gitbrowse() end,                                                              desc = "Open link to line at git remote" },
     -- Zen Mode
-    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>z",        function() Snacks.zen() end,                                                                    desc = "Toggle Zen Mode" },
     -- Picker
-    { "<leader>",  function() end, desc = "" },
-    { "<leader>.",  function() Snacks.picker.files() end, desc = "" },
-    { "<leader><leader>",  function() Snacks.picker.git_files() end, desc = "Git files" },
+    { "<leader>",         function() end,                                                                                 desc = "" },
+    { "<leader>.",        function() Snacks.picker.files() end,                                                           desc = "" },
+    { "<leader><leader>", function() Snacks.picker.git_files() end,                                                       desc = "Git files" },
     -- Find
-    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
-    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    { "<leader>fr",       function() Snacks.picker.recent() end,                                                          desc = "Recent" },
+    { "<leader>fp",       function() Snacks.picker.projects() end,                                                        desc = "Projects" },
     -- Search
-    { "<leader>ss",  function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
-    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    { "<leader>ss",       function() Snacks.picker.grep() end,                                                            desc = "Grep" },
+    { "<leader>sw",       function() Snacks.picker.grep_word() end,                                                       desc = "Visual selection or word",       mode = { "n", "x" } },
+    { "<leader>su",       function() Snacks.picker.undo() end,                                                            desc = "Undo History" },
     -- Diagnostics
-    { "<leader>td", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-    { "<leader>ta", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>td",       function() Snacks.picker.diagnostics_buffer() end,                                              desc = "Buffer Diagnostics" },
+    { "<leader>ta",       function() Snacks.picker.diagnostics() end,                                                     desc = "Diagnostics" },
     -- LSP
-    { "gd", function() Snacks.picker.lsp_definitions({ filter = { paths = { ["react/ts5.0"] = false } } }) end, desc = "Goto Definition" },
-    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "grr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "gd",               function() Snacks.picker.lsp_definitions({ filter = { filter = filter_lsp_definitions } }) end, desc = "Goto Definition" },
+    { "gD",               function() Snacks.picker.lsp_declarations() end,                                                desc = "Goto Declaration" },
+    { "grr",              function() Snacks.picker.lsp_references() end,                                                  nowait = true,                           desc = "References" },
+    { "gI",               function() Snacks.picker.lsp_implementations() end,                                             desc = "Goto Implementation" },
+    { "gy",               function() Snacks.picker.lsp_type_definitions() end,                                            desc = "Goto T[y]pe Definition" },
   },
 }
