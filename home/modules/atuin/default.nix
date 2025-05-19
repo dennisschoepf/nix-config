5c3ff27be9ee913ae9ce5c012936711f2eb47357
Dennis <dennis@dnsc-work.fritz.box>
2025-05-19 11:10:54 +0200

    Updates atuin settings

--- home/modules/atuin/default.nix
+++ home/modules/atuin/default.nix
@@ -2,5 +2,10 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      invert = true;
+      style = "compact";
+      inline_height = 30;
+    };
   };
 }
