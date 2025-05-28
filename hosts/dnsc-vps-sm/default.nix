1dee28461bebf25644d5cfadf12aa94cc87aa857
Dennis <me@dnsc.io>
2025-05-29 00:01:54 +0200

    Configures homepage dashboard

--- hosts/dnsc-vps-sm/default.nix
+++ hosts/dnsc-vps-sm/default.nix
@@ -151,6 +151,94 @@
               icon = "uptime-kuma.png";
             };
           }
+          {
+            "Slides" = {
+              href = "https://uptime.dnsc.io";
+              icon = "slidev.png";
+            };
+          }
+        ];
+      }
+      {
+        "Homelab" = [
+          {
+            "Jellyfin" = {
+              href = "http://100.103.199.4:8096";
+              icon = "jellyfin.png";
+            };
+          }
+          {
+            "Syncthing" = {
+              href = "http://100.103.199.4:8384";
+              icon = "syncthing.png";
+            };
+          }
+        ];
+      }
+    ];
+    bookmarks = [
+      {
+        Tools = [
+          {
+            Fastmail = [
+              {
+                abbr = "FM";
+                href = "https://app.fastmail.com";
+              }
+            ];
+            YNAB = [
+              {
+                abbr = "YN";
+                href = "https://app.ynab.com";
+              }
+            ];
+          }
+        ];
+      }
+      {
+        Dev = [
+          {
+            Codeberg = [
+              {
+                abbr = "CB";
+                href = "https://codeberg.org";
+              }
+            ];
+            Github = [
+              {
+                abbr = "GH";
+                href = "https://github.com/dennisschoepf";
+              }
+            ];
+          }
+        ];
+      }
+      {
+        Entertainment = [
+          {
+            YouTube = [
+              {
+                abbr = "YT";
+                href = "https://youtube.com";
+              }
+            ];
+          }
+          {
+            Twitch = [
+              {
+                abbr = "TW";
+                href = "https://twitch.tv";
+              }
+            ];
+          }
+          {
+            Overcast = [
+              {
+                abbr = "OC";
+                href = "https://overcast.fm";
+              }
+            ];
+          }
         ];
       }
     ];
