9ed96f7142d80599fd12c29c4df55a240048ff68
Dennis <me@dnsc.io>
2025-05-29 21:12:04 +0200

    updates syncthing config

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -4,14 +4,19 @@
     group = "users";
     guiAddress = "0.0.0.0:8384";
     user = "dennis";
-    dataDir = "/home/dennis/sync";
-    # configDir = "/home/dennis/sync/.config/syncthing";
+    configDir = "/home/dennis/.config/syncthing";
     overrideDevices = true;
     overrideFolders = true;
     settings = {
       devices = {
         "dnsc-air" = { id = "RYFO5XN-RRZZQI3-4W5DPHO-C7OKL5N-ZUQUBEI-ZKSWUHM-2BMTFRY-MDC7MA3"; };
         "dnsc-phone" = { id = "NGHODAF-W646NPR-R5IEL2R-QLULKB6-I4APGMB-5PIM6B6-5HZCLAV-HRTSEQI"; };
+      };
+      folders = {
+        "notes" = {
+          path = "/home/dennis/notes";
+          devices = [ "dnsc-air" "dnsc-phone" ];
+        };
       };
     };
   };
