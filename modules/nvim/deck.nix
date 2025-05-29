8a8763d6b677f2abba23e6f9b34975185ae2b3c0
Dennis <me@dnsc.io>
2025-05-29 18:30:24 +0200

    fixes path after module separation

--- modules/nvim/deck.nix
+++ modules/nvim/deck.nix
@@ -4,6 +4,6 @@
   xdg.configFile."nvim" = {
     # mkOutOfStoreSymlink needs an absolute path, otherwise it does not work: https://github.com/nix-community/home-manager/issues/676#issuecomment-1595795685
     # TODO: At least make the location of the config folder dynamic so that it works on other machines
-    source = config.lib.file.mkOutOfStoreSymlink /home/deck/dev/nix-config/home/modules/nvim/config;
+    source = config.lib.file.mkOutOfStoreSymlink /home/deck/dev/nix-config/modules/nvim/config;
   };
 }
--- modules/wezterm/default.nix
+++ modules/wezterm/default.nix
@@ -4,6 +4,6 @@
   xdg.configFile."wezterm" = {
     # mkOutOfStoreSymlink needs an absolute path, otherwise it does not work: https://github.com/nix-community/home-manager/issues/676#issuecomment-1595795685
     # TODO: At least make the location of the config folder dynamic so that it works on other machines
-    source = config.lib.file.mkOutOfStoreSymlink /Users/dennis/dev/nix-config/home/modules/wezterm/config;
+    source = config.lib.file.mkOutOfStoreSymlink /Users/dennis/dev/nix-config/modules/wezterm/config;
   };
 }
