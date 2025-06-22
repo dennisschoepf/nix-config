150dd3ff905247e5230002fbf7212a38f4adfe31
Dennis <me@dnsc.io>
2025-06-22 20:48:06 +0200

    makes ssh paths dynamic

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.ssh = {
     enable = true;
@@ -6,25 +7,25 @@
       "dnsc-server" = {
         hostname = "100.103.199.4";
         user = "dennis";
-        identityFile = "/Users/dennis/.ssh/id_ed25519";
+        identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
         identitiesOnly = true;
       };
       "dnsc-server-i" = {
         hostname = "192.168.178.69";
         user = "dennis";
-        identityFile = "/Users/dennis/.ssh/id_ed25519";
+        identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
         identitiesOnly = true;
       };
       "dnsc-vps-sm" = {
         hostname = "100.65.82.77";
         user = "dennis";
-        identityFile = "/Users/dennis/.ssh/id_ed25519";
+        identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
         identitiesOnly = true;
       };
       "dnsc-storage" = {
         hostname = "u295965.your-storagebox.de";
         user = "u295965";
-        identityFile = "/Users/dennis/.ssh/id_ed25519";
+        identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
         identitiesOnly = true;
         port = 23;
       };
