59bee0701cf8622ac3f9a29ba1eb690a30d18f89
Dennis <me@dnsc.io>
2025-06-22 20:50:03 +0200

    tries to add dynamic config for wezterm

--- modules/wezterm/default.nix
+++ modules/wezterm/default.nix
@@ -3,7 +3,6 @@
 {
   xdg.configFile."wezterm" = {
     # mkOutOfStoreSymlink needs an absolute path, otherwise it does not work: https://github.com/nix-community/home-manager/issues/676#issuecomment-1595795685
-    # TODO: At least make the location of the config folder dynamic so that it works on other machines
-    source = config.lib.file.mkOutOfStoreSymlink /Users/dennis/dev/nix-config/modules/wezterm/config;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dev/nix-config/modules/wezterm/config";
   };
 }
